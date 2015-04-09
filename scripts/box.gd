bebb789eb29621e23a8da8690c0da298de7e9390
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2015-04-10 00:52:24 +0300

    Tilemap update

--- scripts/box.gd
+++ scripts/box.gd
@@ -20,12 +20,14 @@ var check_right = ""
 var check_bottom = ""
 var check_left = ""
 
+var tilemap
 var movement = 0
 var move_right = false
 var move_left = false
 
 func _ready():
 	set_fixed_process(true)
+	tilemap = get_node("../tilemap")
 	ray_top = get_node("ray_top")
 	ray_right = get_node("ray_right")
 	ray_bottom = get_node("ray_bottom")
@@ -45,23 +47,24 @@ func _ready():
 
 func _fixed_process(delta):
 	if movement == 0:
+		var current_position = get_pos()/64
 		#fall
-		if !ray_bottom.is_colliding():
+		if !ray_bottom.is_colliding() && tilemap.get_cell(current_position.x, current_position.y + 1) == -1:
 			move(Vector2(0,4))
 			collider_bottom = ""
-
-		#i have to simplify this
-		if ray_bottom.is_colliding():
+		else:
 			#sink
-			collider_bottom = ray_bottom.get_collider().get_name()
-			if collider_bottom.substr(0,4) == "acid":
-				move(Vector2(0,1))
+			if(ray_bottom.is_colliding()):
+				collider_bottom = ray_bottom.get_collider().get_name()
+				if collider_bottom.substr(0,4) == "acid":
+					move(Vector2(0,1))
 			#move left
 			if ray_check_left.is_colliding() and move_right:
 				collider_left = ray_check_left.get_collider().get_name()
 				if collider_left == "player":
 					if Input.is_action_pressed("btn_right"):
 						movement = 64
+						ray_check_left.get_collider().movement = ray_check_left.get_collider().movement + 64
 			else:
 				collider_left = ""
 				
@@ -71,13 +74,14 @@ func _fixed_process(delta):
 				if collider_right == "player":
 					if Input.is_action_pressed("btn_left"):
 						movement = -64
+						ray_check_right.get_collider().movement = ray_check_right.get_collider().movement - 64
 			else:
 				collider_right = ""
 				
 			#check left
 			if ray_check_left.is_colliding():
 				check_left = ray_check_left.get_collider().get_name()
-				if check_left.substr(0,6) == "ground" or check_left.substr(0,3) == "box":
+				if tilemap.get_cell(current_position.x - 1, current_position.y) == 0 or check_left.substr(0,3) == "box":#!!!
 					move_left = false
 				else:
 					move_left = true
@@ -88,7 +92,7 @@ func _fixed_process(delta):
 			#check right
 			if ray_check_right.is_colliding():
 				check_right = ray_check_right.get_collider().get_name()
-				if check_right.substr(0,6) == "ground" or check_right.substr(0,3) == "box":
+				if tilemap.get_cell(current_position.x + 1, current_position.y) == 0 or check_right.substr(0,3) == "box":
 					move_right = false
 				else:
 					move_right = true
--- scripts/player.gd
+++ scripts/player.gd
@@ -27,12 +27,17 @@ var check_overlap = ""
 var movement = 0
 var move_left = false
 var move_right = false
+var move_down = false
 
+var tilemap
+var current_position
+export var TILE_LADDER = 1
 var move_up = 0
+var collider_name
 
 func _ready():
 	set_fixed_process(true)
-	
+	tilemap = get_node("../tilemap")
 	ray_top = get_node("ray_top")
 	ray_right = get_node("ray_right")
 	ray_bottom = get_node("ray_bottom")
@@ -57,69 +62,62 @@ func _ready():
 func _fixed_process(delta):
 	
 	if movement == 0 and move_up == 0:
+		current_position = get_pos()/64
 		#allow to move right
+		check_right = tilemap.get_cell(current_position.x + 1, current_position.y)
+		move_right = (check_right == -1 || check_right == TILE_LADDER)
 		if ray_check_right.is_colliding():
-			check_right = ray_check_right.get_collider().get_name()
-			if check_right.substr(0,6) == "ground" or check_right.substr(0,3) == "box":
+			var collider_name = ray_check_right.get_collider().get_name()
+			if collider_name.substr(0,3) == "box":
 				move_right = false
-			else:
-				move_right = true
-		else:
-			check_right =  ""
-			move_right = true
-		
 		#allow to move left
+		check_left = tilemap.get_cell(current_position.x - 1, current_position.y)
+		move_left = (check_left == -1 || check_left == TILE_LADDER)
 		if ray_check_left.is_colliding():
-			check_left = ray_check_left.get_collider().get_name()
-			if check_left.substr(0,6) == "ground" or check_left.substr(0,3) == "box":
+			var collider_name = ray_check_left.get_collider().get_name()
+			if collider_name.substr(0,3) == "box":
 				move_left = false
-			else:
-				move_left = true
-		else:
-			check_left =  ""
-			move_left = true
-			
-				#check overlap
-		if ray_overlap.is_colliding():
-			check_overlap = ray_overlap.get_collider().get_name()
-		else:
-			check_overlap = ""
-			
+		#check overlap
+		check_overlap = tilemap.get_cell(current_position.x, current_position.y)
 		#check down
+		check_bottom = tilemap.get_cell(current_position.x, current_position.y + 1)
+		move_down = (check_bottom == -1)
 		if ray_check_bottom.is_colliding():
-			check_bottom = ray_check_bottom.get_collider().get_name()
-		else:
-			check_bottom = ""
+			var collider_name = ray_check_bottom.get_collider().get_name()
+			if collider_name.substr(0,3) == "box":
+				move_down = false
 	
 		#ask to move right
-		if ray_bottom.is_colliding() and move_right:
+		if !move_down and move_right:
 			if Input.is_action_pressed("btn_right"):
 				movement = 64
 				
 		#ask to move left
-		if ray_bottom.is_colliding() and move_left:
+		if !move_down and move_left:
 			if Input.is_action_pressed("btn_left"):
 				movement = -64
 				
 		#ask to climb
-		if check_overlap.substr(0,6) == "ladder":
+		if check_overlap == TILE_LADDER:
 			if Input.is_action_pressed("btn_up"):
 				move_up = 64
 				
 		#ask to lower
-		if check_bottom.substr(0,6) == "ladder":
+		if check_bottom == TILE_LADDER:
 			if Input.is_action_pressed("btn_down"):
 				move_up = -64
 
 		#fall
-		if !ray_bottom.is_colliding():
+		if move_down || int(get_pos().y)%64 > 0:
 			move(Vector2(0,4))
 	
 	else:
 		if movement > 0:
+			get_node("Sprite").set_flip_h(false)
 			movement -= 4
 			move(Vector2(4,0))
 		elif movement < 0:
+			get_node("Sprite").set_flip_h(true)
 			movement += 4
 			move(Vector2(-4,0))
 		if move_up > 0:
@@ -128,3 +126,4 @@ func _fixed_process(delta):
 		elif move_up < 0:
 			move_up += 4
 			move(Vector2(0,4))
+
