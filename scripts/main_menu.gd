9cc14b91f62787ff140d4db220f41577da15d2e3
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2015-04-10 19:03:22 +0300

    Made a bit better menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,29 +1,47 @@
 extends Control
 
 var select_level
-var run_once = true
+var select_pack
 
 func _ready():
 	select_level = get_node("opt_level")
+	select_pack = get_node("opt_pack")
 	
 	var diraccess = Directory.new()
-	diraccess.open("res://levels/tutorial/")
+	diraccess.open("res://levels/")
 
 	diraccess.list_dir_begin()
 	var name = diraccess.get_next()
+	var i = 1
+	while name:
+		if diraccess.current_is_dir():
+			if name.length() > 3:
+				select_pack.add_item(name)
+		name = diraccess.get_next()
+	diraccess.list_dir_end()
+	_on_opt_pack_item_selected( 0 )
 
+func _on_opt_pack_item_selected( ID ):
+	select_level.clear()
+	var diraccess = Directory.new()
+	diraccess.open(str("res://levels/", select_pack.get_text()))
+
+	diraccess.list_dir_begin()
+	var name = diraccess.get_next()
+	var i = 1
 	while name:
 		if !diraccess.current_is_dir():
 			if name.length() > 3:
-				select_level.add_item(name)
+				select_level.add_item(str(i))
+				i = i + 1
 		name = diraccess.get_next()
 	diraccess.list_dir_end()
-		
+
+func _on_btn_play_pressed():
 	set_fixed_process(true)
-	
+
 func _fixed_process(delta):
-	if get_node("btn_play").is_pressed():
-		if run_once:
-			run_once = false
-			var text = select_level.get_text()
-			get_node("/root/global").load_level("tutorial",text.substr(text.length()-5,1))
+	get_node("/root/global").load_level("tutorial",select_level.get_text())
+	set_fixed_process(false)
+	
+
