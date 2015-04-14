eb2b130e0c12bd21a441a8c4e97fc556a4a311bc
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2015-04-14 21:04:08 +0300

    Added box and flower counters. Closes #33

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -2,11 +2,11 @@ extends StaticBody2D
 # This script maneges flowers
 
 func _ready():
-	get_node("../../../level_holder").goal_add()
+	get_node("../../../level_holder").goal_add("flower")
 	pass
 
 func destroy(var by): # Called whenever the flower is destroyed
 	if(by != "bomb"): # When we weren't demolished by a bomb
-		get_node("../../../level_holder").goal_take()
+		get_node("../../../level_holder").goal_take("flower")
 		queue_free() # delete the flower from the scene
 
