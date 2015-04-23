e1e813298d56d72ad974b4d302f1db335db632d7
fry- <fry-@users.noreply.github.com>
2015-04-23 19:35:58 +0200

    change breakable tile and add animation

--- scripts/breakable_ground.gd
+++ scripts/breakable_ground.gd
@@ -6,9 +6,12 @@ extends StaticBody2D
 
 var player_class = preload("res://scripts/player.gd")
 var passed = false
+var delay = -1
 var ray_check_top
 
 func _ready():
+	get_node("Sprite").set_opacity(1) ### REMOVE if not needed
+	get_node("Sprite").set_pos(Vector2(0,-8)) ### REMOVE if not needed
 	ray_check_top = get_node("ray_check_top")
 	ray_check_top.add_exception(self)
 	set_fixed_process(true)
@@ -18,5 +21,6 @@ func _fixed_process(delta):
 		if(ray_check_top.get_collider() extends player_class): 
 			passed = true
 	elif(passed == true):
+		get_node("Sprite/AnimationPlayer").play("destroy") ### REMOVE if not needed
 		queue_free()
 
