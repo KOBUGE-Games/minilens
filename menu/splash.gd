20adbe189616fa27fcadb95907921d57a5ea2f55
Rémi Verschelde <rverschelde@gmail.com>
2016-04-17 14:48:26 +0200

    Secret button to change splash

--- menu/splash.gd
+++ menu/splash.gd
@@ -1,12 +1,14 @@
 
-extends Label
+extends Node2D
 
 export(String, FILE) var splashes_path = "res://splashes.txt"
 
 onready var splashes = FileManager.get_file_lines(splashes_path)
 
 func _ready():
+	set_random_splash()
+	get_node("../splash_reload").connect("pressed", self, "set_random_splash")
+
+func set_random_splash():
 	var random = randi() % splashes.size()
-	set_text(splashes[random])
-
-
+	get_node("label").set_text(splashes[random])
