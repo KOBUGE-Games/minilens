e7e478c5288ff322da0c8540ff1b7f1e8d39d916
Rémi Verschelde <rverschelde@gmail.com>
2016-04-17 15:26:29 +0200

    Add some new splashes
    Also strip empty lines from splashes

--- menu/splash.gd
+++ menu/splash.gd
@@ -6,6 +6,13 @@ export(String, FILE) var splashes_path = "res://splashes.txt"
 onready var splashes = FileManager.get_file_lines(splashes_path)
 
 func _ready():
+	# Strip empty newlines
+	var splashes_new = []
+	for line in splashes:
+		if line.strip_edges() != "":
+			splashes_new.append(line)
+	splashes = splashes_new
+	
 	set_random_splash()
 	get_node("../splash_reload").connect("pressed", self, "set_random_splash")
 
