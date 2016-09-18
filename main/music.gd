348d648be4ce22bca82cae0ee976b4aade00771a
ObaniGemini <robin@pi-et-ro.net>
2016-09-18 20:09:39 +0200

    Add better music support Music isn't stopped at each level_load but loops until the player quits

--- main/music.gd
+++ main/music.gd
@@ -6,7 +6,10 @@ func play_random_music():
 	if SettingsManager.get_settings().music:
 		var stream_player = get_child(randi() % get_child_count())
 		stream_player.play()
+		stream_player.connect("finished", self, "play_random_music")
 
 func stop_music():
 	for child in get_children():
 		child.stop()
+		if child.is_connected("finished", self, "play_random_music"):
+			child.disconnect("finished", self, "play_random_music")
