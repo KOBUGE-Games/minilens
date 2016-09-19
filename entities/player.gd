a1f2d70bb8643b96ef06ec1dad3ac9948273e8d9
ObaniGemini <robin@pi-et-ro.net>
2016-09-19 18:36:34 +0200

    Juicier lookaround
    - Add tweens and animations to smooth the "in and out"
    - Remove the pause-frame that was really not good looking
    - Add a can_move bool instead in player_class (will permit better "in and out" animations for the future with the player)

--- entities/player.gd
+++ entities/player.gd
@@ -9,6 +9,8 @@ var old_animation = ""
 var bombs = 0
 var did_place_bomb = false
 
+var can_move = true
+
 onready var character = get_node("character")
 onready var animation_player = get_node("animation_player")
 onready var camera = get_node("camera")
@@ -50,39 +52,39 @@ func next_move():
 	
 	if tile_types.overlap == TileConfig.TILE_SINK:
 		destroy()
-	
-	if Input.is_action_pressed("btn_right") and can_move_in_direction("right", false, true):
-		move_in_direction("right", false, true)
-		current_animation = "walk"
-		turn = true
+	if can_move:
+		if Input.is_action_pressed("btn_right") and can_move_in_direction("right", false, true):
+			move_in_direction("right", false, true)
+			current_animation = "walk"
+			turn = true
 
-	elif Input.is_action_pressed("btn_left") and can_move_in_direction("left", false, true):
-		move_in_direction("left", false, true)
-		current_animation = "walk"
-		turn = true
+		elif Input.is_action_pressed("btn_left") and can_move_in_direction("left", false, true):
+			move_in_direction("left", false, true)
+			current_animation = "walk"
+			turn = true
 
-	elif Input.is_action_pressed("btn_up") and can_move_in_direction("top", false, true) and tile_types["overlap"] == TileConfig.TILE_CLIMB:
-		move_in_direction("top", false, true)
-		current_animation = "climb"
-		turn = true
+		elif Input.is_action_pressed("btn_up") and can_move_in_direction("top", false, true) and tile_types["overlap"] == TileConfig.TILE_CLIMB:
+			move_in_direction("top", false, true)
+			current_animation = "climb"
+			turn = true
 
-	elif Input.is_action_pressed("btn_down") and can_move_in_direction("bottom", false, true) \
-			and (tile_types["overlap"] == TileConfig.TILE_CLIMB or tile_types["bottom"] == TileConfig.TILE_CLIMB):
-		move_in_direction("bottom", false, true)
-		current_animation = "climb"
-		turn = true
+		elif Input.is_action_pressed("btn_down") and can_move_in_direction("bottom", false, true) \
+				and (tile_types["overlap"] == TileConfig.TILE_CLIMB or tile_types["bottom"] == TileConfig.TILE_CLIMB):
+			move_in_direction("bottom", false, true)
+			current_animation = "climb"
+			turn = true
 
-	elif Input.is_action_pressed("place_bomb") and bombs > 0:
-		if not did_place_bomb:
-			bombs -= 1
-			level_holder.emit_signal("counters_changed")
-			var bomb = bomb_scene.instance()
-			level_holder.level_node.add_child(bomb)
-			bomb.set_global_pos(get_global_pos())
-			level_holder.turn()
-		did_place_bomb = true
-	else:
-		did_place_bomb = false
+		elif Input.is_action_pressed("place_bomb") and bombs > 0:
+			if not did_place_bomb:
+				bombs -= 1
+				level_holder.emit_signal("counters_changed")
+				var bomb = bomb_scene.instance()
+				level_holder.level_node.add_child(bomb)
+				bomb.set_global_pos(get_global_pos())
+				level_holder.turn()
+			did_place_bomb = true
+		else:
+			did_place_bomb = false
 	
 	if turn:
 		if pause_frames <= 1:
--- main/lookaround.gd
+++ main/lookaround.gd
@@ -6,19 +6,25 @@ var last_mouse_pos = Vector2()
 var preenable_top_margin = 0
 
 onready var player_camera = get_node("../../player_holder/player/camera")
+onready var tween = get_node("Tween")
+onready var animation_player = get_node("AnimationPlayer")
 
 func _ready():
 	set_enabled(false)
 
 func set_enabled(enabled):
-	get_tree().call_deferred("set_pause", enabled)
-	set_hidden(!enabled)
 	player_camera.set_offset(Vector2(0, 0))
 	var z = 0.24
+	var camera_zoom = player_camera.get_zoom()
 	var scale_factor = Vector2(enabled*z + 1, enabled*z + 1)
-	player_camera.set_zoom(scale_factor)
+	
+	tween.interpolate_property(player_camera, "zoom", camera_zoom, scale_factor, 0.5, 1, 1)
+	tween.start()
 	
 	if enabled:
+		player_camera.get_parent().can_move = false
+		animation_player.play("enabled")
+		
 		preenable_top_margin = player_camera.get_limit(MARGIN_TOP)
 		var distance = player_camera.get_limit(MARGIN_BOTTOM) - player_camera.get_limit(MARGIN_TOP)
 		var space = get_node("/root/ScreenManager").initial_size.y
@@ -28,7 +34,15 @@ func set_enabled(enabled):
 		
 		player_camera.set_limit(MARGIN_TOP, player_camera.get_limit(MARGIN_TOP) - 60) # Makes things feel less rigid
 	else:
-		player_camera.set_limit(MARGIN_TOP, preenable_top_margin)
+		player_camera.get_parent().can_move = true
+		if camera_zoom > Vector2(1, 1):
+			animation_player.play("disabled")
+			animation_player.connect("finished", self, "set_disabled")
+
+func set_disabled():
+	if animation_player.is_connected("finished", self, "set_disabled"):
+		animation_player.disconnect("finished", self, "set_disabled")
+	player_camera.set_limit(MARGIN_TOP, preenable_top_margin)
 
 func _input_event(event):
 	
